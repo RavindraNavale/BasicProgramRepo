6e0669c530ce0b958603c006126bb689b4b2f462
Ravindra <ravinavale92@gmail.com>
2021-01-22 12:42:10 +0530

    Inital commit

---
+++ Practice/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Parent
+    {
+        public void tt()
+        {
+            Console.WriteLine("method parent");
+            Console.ReadLine();
+        }
+    }
+
+    public class child : Parent
+    {
+        public void  tt()
+        {
+            Console.WriteLine("method child");
+            Console.ReadLine();
+        }
+
+    }
+}
